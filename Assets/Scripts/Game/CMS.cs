b00d3acaf20c9fafbed658e6b5ca0f7e884ce9d7
Aycan <aycan.acn2020@gmail.com>
2024-01-14 15:34:53 +0100

    add more pear and apples.

--- Assets/Scripts/Game/CMS.cs
+++ Assets/Scripts/Game/CMS.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -62,6 +63,7 @@ namespace Game
             
                 toggle.interactable = false;
                 toggle.isOn = true;
+                
             }
         }
 
